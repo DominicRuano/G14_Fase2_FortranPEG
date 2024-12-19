09cc65a0adb98d4bddaaafcf79e4cb5ae29484f1
Luis de León <123606166+Luisdelt@users.noreply.github.com>
2024-12-19 02:02:38 -0600

    Generator

---
+++ src/visitor/Node.js
@@ -0,0 +1,5 @@
+export default class Node{
+    accept(visitor){
+        throw new Error("Error en la clase nodo")
+    }
+}
---
+++ tools/Nodes.js
@@ -0,0 +1,9 @@
+const nodes = {
+    Producciones: ['id', 'expr', 'alias'],
+    Opciones: ['exprs'],
+    Union: ['exprs'],
+    Expresion: ['expr', 'label', 'quiantifier'],
+    String: ['val', 'isCase'],
+};
+
+export default nodes;
